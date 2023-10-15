2943c2f62d0c6b2154b913a5651846a49fc43156
erode <2724794017@qq.com>
2023-10-15 08:32:55 +0800

    Site updated: 2023-10-15 08:32:54

--- gallery/photos/js/demo.js
+++ gallery/photos/js/demo.js
@@ -1,4 +1,4 @@
-function Index(node) {
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script>function Index(node) {
     this.node = node;
     this.init();
 };
